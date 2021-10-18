bde431ddd469a3674867ced49dd0744510d8087a
yegorlitsov <yegorlitsov@gmail.com>
2021-10-18 10:17:02 +0300

    some updates for animation and "add customer" busson added

---
+++ src/app/modules/customer-dashboard/modules/customer-dashboard-board/components/customer-dashboard-add-customer/customer-dashboard-add-customer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-dashboard-add-customer',
+  templateUrl: './customer-dashboard-add-customer.component.html',
+  styleUrls: ['./customer-dashboard-add-customer.component.scss']
+})
+export class CustomerDashboardAddCustomerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/modules/customer-dashboard/modules/customer-dashboard-board/customer-dashboard-board.module.ts
+++ src/app/modules/customer-dashboard/modules/customer-dashboard-board/customer-dashboard-board.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CustomerDashboardBoardComponent } from './customer-dashboard-board.component';
 import { CustomerDashboardCardComponent } from './components/customer-dashboard-card/customer-dashboard-card.component';
+import { CustomerDashboardAddCustomerComponent } from './components/customer-dashboard-add-customer/customer-dashboard-add-customer.component';
 
 
 
 @NgModule({
   declarations: [
     CustomerDashboardBoardComponent,
-    CustomerDashboardCardComponent
+    CustomerDashboardCardComponent,
+    CustomerDashboardAddCustomerComponent
   ],
   exports: [
     CustomerDashboardBoardComponent
